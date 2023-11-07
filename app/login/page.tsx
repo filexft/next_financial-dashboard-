4727b9773f16207039a4c4d101142e669f6512ba
filexft <filimonteweldeft@gmail.com>
2023-11-07 16:56:12 +0100

    start of commit

---
+++ app/login/page.tsx
@@ -0,0 +1,18 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+
+import Link from 'next/link';
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        {/* <AcmeLogo /> */}
+      </div>
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+/>
+          <h1>hello login</h1>
+      </div>
+    </main>
+  );
+}
